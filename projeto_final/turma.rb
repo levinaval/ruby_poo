437f0ac3b80258479c9f9a99e522edd4637b05ee
levi <levisantoluna@gmail.com>
2021-02-01 20:09:10 -0300

    fezendo rebase

--- projeto_final/turma.rb
+++ projeto_final/turma.rb
@@ -1,21 +1,33 @@
-class Turma < Pessoa
-    attr_accessor :nome_da_turma
-    
-    def initialize(nome_da_turma)
-        @nome_da_turma = nome_da_turma
+class Turma
+    attr_accessor :nome_da_turma, :professor, :disciplina
+
+    def initialize(nome_da_turma, professor, disciplina)
+       @nome_da_turma = nome_da_turma
+       @professor = professor
+       @disciplina = disciplina
     end
+
+   #  def lista
+   #     lista = []
+   #      for i in 0..0
+   #        nome = "levi, luna, carlos, nicolas, artur, marcos, marcelo, laura, tiago, cintia"
+   #        lista.push(nome)  
+   #      end
+   #  end
+   def lista
+      lista = []
+       for i in "levi, luna, carlos, nicolas, artur, marcos, marcelo, laura, tiago, cintia"
+         
+         lista.push(nome)  
+       end
+   end
 
     def apresentar
-        puts "olá! eu sou o professor #{professor} dou aula de #{disciplina}. e estas são as minhas turmas
-        #{turma1}, #{matricula1} e #{turma2}, #{matricula2}"
-    end
-
-    def turma1
-        puts "incira os nomes dos alunos."
-        lista[]
-
-        lista.push ("joão, levi") 
-        lista << gets.chomp
-         puts lista [1..9] -
-    end
+        puts "olá! nos somos da turma #{nome_da_turma} eu sou o professor #{professor} da disciplina #{disciplina} e estes são os meus alunos #{lista}, e sua matrícula "
+     end
 end
+
+tru1 = Turma.new("alfa", "diego", "progamação")
+
+tru1.apresentar
+puts lista
