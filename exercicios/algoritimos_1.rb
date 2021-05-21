0736c7b53423ed8f114c6cba8d9845682328e958
levi <levisantosluna@gmail.com>
2021-05-21 05:04:17 -0300

    A melhora

--- exercicios/algoritimos_1.rb
+++ exercicios/algoritimos_1.rb
@@ -1,17 +1,3 @@
-#lista = []
-#for i in 1..5
-#  numero = gets.chomp.to_i
-#  lista.push(numero)  
-#end
-#print lista
-
-#resultado = 0
-#for numero in lista
-#  resultado = resultado + numero
-#end
-#puts resultado
-
-#RESOLVIDA POR DIEGO
 lista = []
 resultado = 0
 
