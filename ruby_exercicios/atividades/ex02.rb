b7062fb05fd8f556452314d4359739371c1a5264
levi <levisantosluna@gmail.com>
2021-06-21 20:26:43 -0300

    exercicios

---
+++ ruby_exercicios/atividades/ex02.rb
@@ -0,0 +1,16 @@
+#Escreva um algoritmo que solicite ao usuário a entrada de 5 nomes, e que exiba a lista desses nomes na tela.
+#Após exibir essa lista, o programa deve mostrar também os nomes na ordem inversa em que o usuário os digitou, um por linha.
+
+lista = []
+for i in 1..5
+  nome = gets.chomp
+  lista.push(nome)  
+end
+print lista
+puts ""
+
+contador = lista.size - 1
+while contador >= 0
+  puts lista[contador]
+  contador -= 1
+end
