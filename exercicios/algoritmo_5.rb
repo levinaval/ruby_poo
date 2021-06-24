54049b2280b722da7fee1efb759fed0193d2f800
levi <levisantosluna@gmail.com>
2021-06-24 19:24:04 -0300

    praticando

--- exercicios/algoritmo_5.rb
+++ exercicios/algoritmo_5.rb
@@ -1,2 +1,13 @@
 #Crie um programa que solicite a entrada de 10 números pelo usuário, armazenando-os em um vetor, e então monte outro vetor com os valores do primeiro multiplicados por 5.
 #Exiba os valores dos dois vetores na tela, simultaneamente, em duas colunas (um em cada coluna), uma posição por linha.
+
+lista1 = [0..5]
+lista2 = [0..5]
+# count = inteiro
+
+puts "digite  número."
+for i in 0..5
+  numero = gets.chomp
+  lista1 << lista2 * 5
+end
+
