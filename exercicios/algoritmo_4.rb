2457b2fd5ad27bd9af5a54b07aed0fe003117cb2
levi <levisantosluna@gmail.com>
2021-06-25 23:29:50 -0300

    praticando

--- exercicios/algoritmo_4.rb
+++ exercicios/algoritmo_4.rb
@@ -1,5 +1,5 @@
 #Escreva um algoritmo que solicite ao usuário a entrada de 5 nomes, e que exiba a lista desses nomes na tela.
-#Após exibir essa lista, o programa deve mo  strar também os nomes na ordem inversa em que o usuário os digitou, um por linha.
+#Após exibir essa lista, o programa deve mostrar também os nomes na ordem inversa em que o usuário os digitou, um por linha.
 
 lista = []
 
--- exercicios/algoritmo_5.rb
+++ exercicios/algoritmo_5.rb
@@ -1,13 +1,21 @@
 #Crie um programa que solicite a entrada de 10 números pelo usuário, armazenando-os em um vetor, e então monte outro vetor com os valores do primeiro multiplicados por 5.
 #Exiba os valores dos dois vetores na tela, simultaneamente, em duas colunas (um em cada coluna), uma posição por linha.
 
-lista1 = [0..5]
-lista2 = [0..5]
+lista1 = []
+lista2 = []
 # count = inteiro
 
 puts "digite dez número."
-for i in 0..5
+for i in 0..3
   numero = gets.chomp
   lista1 << lista2 * 5
 end
 
+puts " "
+puts "resultado:"
+
+for i in 0..1
+  puts "#{lista1}, #{lista2}"
+end
+
+puts " "
