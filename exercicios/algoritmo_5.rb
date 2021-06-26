0c8f0bb90e1c9c8d6d99701beaf3aa915ad0def7
levi <levisantosluna@gmail.com>
2021-06-26 19:46:44 -0300

    vogal ou consoante

--- exercicios/algoritmo_5.rb
+++ exercicios/algoritmo_5.rb
@@ -1,4 +1,4 @@
-#Crie um programa que solicite a entrada de 10 números pelo usuário, armazenando-os em um vetor, e então monte outro vetor com os valores do primeiro multiplicados por 5.
+#Crie um programa que solicite a entrada de 10 números pelo usuário, armazenando-os em um vetor, e então monte outro vetor, com os valores do primeiro multiplicados por 5.
 #Exiba os valores dos dois vetores na tela, simultaneamente, em duas colunas (um em cada coluna), uma posição por linha.
 
 lista1 = []
@@ -8,7 +8,7 @@ lista2 = []
 puts "digite dez número."
 for i in 0..3
   lista1 = gets.chomp
-  lista2 << lista1 * 5
+  lista2 << (lista1) * 5
 end
 
 puts "resultado:"
---
+++ exercicios/algoritmo_6.rb
@@ -0,0 +1,1 @@
+#Crie um programa que solicite um nome e verifique se começa com uma vogal ou consoante.
