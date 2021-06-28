2fde4e3cd6ee87fd21062a83c47ac66fc0e508b3
levi <levisantosluna@gmail.com>
2021-06-28 19:21:55 -0300

    5 numeros de forma aleatoria e ordem crescente

--- exercicios/algoritmo_7.rb
+++ exercicios/algoritmo_7.rb
@@ -1,9 +1,9 @@
 #Crie um programa que receba 5 números positivos de forma aleatória, e os apresente em ordem crescente.
 
-puts "for"
+puts "random numbers"
 numeros = [6, 5, 2, 7, 9]
 numeros_organisados = numeros.sort
-puts "while"
+puts "numbers ascending order"
 cont = numeros_organisados.size - 1
  while cont >= 0
   puts numeros_organisados[cont]
