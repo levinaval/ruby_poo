d92406d0ea6ebe99ce80ff38247c5e18e81e1caf
levi <levisantosluna@gmail.com>
2021-06-30 16:41:59 -0300

    numero maior

--- exercicios/algoritmo_8.rb
+++ exercicios/algoritmo_8.rb
@@ -1,8 +1,8 @@
 #Escreva um programa (algoritmo) que receba dois números e mostre qual é o maior deles.
 
-puts "insira o valor 1."
+puts "insira o Primeiro valor."
 valor1 = gets.chomp.to_i
-puts "insira o valor 2."
+puts "insira o Segundo valor."
 valor2 = gets.chomp.to_i
 if  valor1 > valor2
   puts "#{valor1} é o maior!"
