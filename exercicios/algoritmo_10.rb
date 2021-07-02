47ba61771a54083be04ba10a8da0cab0dbb8dbc3
levi <levisantosluna@gmail.com>
2021-07-02 16:27:14 -0300

    numeros pares

--- exercicios/algoritmo_10.rb
+++ exercicios/algoritmo_10.rb
@@ -2,18 +2,29 @@
 #Entrada: 3, 6, 4, 9, 1
 #Saída: 6, 4
 
-lista = []
+#lista = []
 
-puts "incira cinco números"
+#puts "incira cinco números"
 
-for i in 0..4
-  n = gets.chomp.to_i
-  lista.push(n)
-end
-puts "-----------"
+#for i in 0..4
+#  n = gets.chomp.to_i
+#  lista.push(n)
+#end
+#puts "-----------"
 
-for i in lista
-  if i % 2 == 0
-    puts i
+#for i in lista
+#  if i % 2 == 0
+#    puts i
+#  end
+#end
+
+                # ou
+
+
+def numeros_pares(lista)
+  for i in lista
+    if i % 2 == 0
+     puts i
+    end
   end
 end
