2f3d8c4422b414422747f3aed34fd6c25ec3828d
levi <levisantosluna@gmail.com>
2021-07-02 16:20:59 -0300

    numeros pares

--- exercicios/algoritmo_10.rb
+++ exercicios/algoritmo_10.rb
@@ -1,3 +1,11 @@
 #Escreva um programa (algoritmo) que receba cinco números e imprima somente os pares.
 #Entrada: 3, 6, 4, 9, 1
 #Saída: 6, 4
+
+lista = []
+
+for i in 0..4
+  n = gets.chomp.to_i
+  lista.push(n)
+end
+puts "-----------"
