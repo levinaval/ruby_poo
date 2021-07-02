fc26455eaa6236daec894e4642e091dc7198ddaf
levi <levisantosluna@gmail.com>
2021-07-01 22:12:51 -0300

    recebe tres numeros e sai o maior

--- exercicios/algoritmo_9.rb
+++ exercicios/algoritmo_9.rb
@@ -1,3 +1,19 @@
 #Escreva um programa (algoritmo) que receba três números e mostre qual é o maior deles.
 #Entrada: 3, 4, 9
 #Saída: 9 é o maior
+
+puts "insira o valor 1."
+valor1 = gets.chomp.to_i
+puts "insira o valor 2."
+valor2 = gets.chomp.to_i
+puts "insira o valor 3."
+valor3 = gets.chomp.to_i
+  if  valor1 > valor2 and valor1 > valor3
+    puts "#{valor1} é o maior!"
+  elsif  valor2 > valor1 and valor2 > valor3
+    puts "#{valor2} é o maior!"
+  elsif  valor3 > valor2 and valor3 > valor1
+    puts "#{valor3} é o maior!"
+  else
+    puts "valores são iquais!"
+end
