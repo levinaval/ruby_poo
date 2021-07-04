2209400a5cbea12fce5717e60df51f45f545126f
levi <levisantosluna@gmail.com>
2021-07-03 22:23:46 -0300

    numeros inpares

--- exercicios/algoritmo_11.rb
+++ exercicios/algoritmo_11.rb
@@ -2,18 +2,30 @@
 #Entrada: 3, 6, 4, 9, 1
 #Saída: 3, 9, 1
 
-lista = []
+#lista = []
 
-puts "digite cinco números"
+#puts "digite cinco números"
 
-for i in 0..4
-  n = gets.chomp.to_i
-  lista.push(n)
-end
-puts "-----------"
+#for i in 0..4
+#  n = gets.chomp.to_i
+#  lista.push(n)
+#end
+#puts "-----------"
 
-for i in lista
-  if i % 2 == 1 #ou if i % 2 != 0
-    puts i
-  end
-end
+#for i in lista
+#  if i % 2 == 1 #ou if i % 2 != 0
+#    puts i
+#  end
+#end
+
+  # ou
+
+  def numeros_pares(lista)
+    for i in lista
+      if i % 2 != 0
+       puts i
+      end
+    end
+   end
+   
+   
