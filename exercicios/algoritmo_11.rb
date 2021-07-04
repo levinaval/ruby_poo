029e33714f69117feed0d37f2dc114b000459eab
levi <levisantosluna@gmail.com>
2021-07-03 22:18:00 -0300

    numeros inpares

--- exercicios/algoritmo_11.rb
+++ exercicios/algoritmo_11.rb
@@ -1,3 +1,11 @@
 #Escreva um programa (algoritmo) que receba cinco números e imprima somente os ímpares.
 #Entrada: 3, 6, 4, 9, 1
 #Saída: 3, 9, 1
+
+lista = []
+
+for i in 0..4
+  n = gets.chomp.to_i
+  lista.push(n)
+end
+puts "-----------"
