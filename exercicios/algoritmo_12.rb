aa27d10b0f8f47c8f4580861bf332eda07e8eb42
levi <levisantosluna@gmail.com>
2021-07-05 20:35:05 -0300

    aria do quadrado

--- exercicios/algoritmo_12.rb
+++ exercicios/algoritmo_12.rb
@@ -2,7 +2,14 @@
 #Entrada: 3, 3
 #Saída: 9m²
 
-puts "insira o valos do quadrado."
-  l = gets.chomp.to_f
-  quadrado = l*l # l**2
-  puts "ao valor é #{quadrado}, m²!"
+#puts "insira o valos do quadrado."
+#  l = gets.chomp.to_f
+#  quadrado = l*l # l**2
+#  puts "ao valor é #{quadrado}, m²!"
+
+#OU
+
+def area_quadrado(lado)
+  lado * lado # lado**2
+end
+
