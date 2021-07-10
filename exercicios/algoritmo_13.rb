3a2783328340d15980c11eef16c4da80fb3d381b
levi <levisantosluna@gmail.com>
2021-07-09 21:18:33 -0300

    area do retangulo

--- exercicios/algoritmo_13.rb
+++ exercicios/algoritmo_13.rb
@@ -1,3 +1,10 @@
 #Escreva um programa que calcule a área de um retângulo.
 #Entrada: 3, 4
 #Saída: 12m²
+
+puts "insira o primeiro valor."
+l1 = gets.chomp.to_i
+puts "insira o segundo valor."
+l2 = gets.chomp.to_i
+retangulo = l1*l2
+puts "ao valor do retângulo é #{retangulo}m²!"
