e406ba32e4bb2a9fc4567166a9de17c00ba9b444
levi <levisantosluna@gmail.com>
2021-07-10 23:30:34 -0300

    nedia aritmetica

--- exercicios/algoritmo_14.rb
+++ exercicios/algoritmo_14.rb
@@ -1,3 +1,11 @@
 #Escreva um algoritmo que receba o nome de um aluno e quatro notas e faça a média aritmética.
 #Entrada: 6.8, 9.5, 3.9, 4.2
 #Saída: Aluno: Fulano, Média: 6,1
+
+puts "insira o nome do aluno e as notas."
+aluno = gets.chomp
+puts "notas." 
+nota1 = gets.chomp.to_f
+nota2 = gets.chomp.to_f
+nota3 = gets.chomp.to_f
+nota4 = gets.chomp.to_f
