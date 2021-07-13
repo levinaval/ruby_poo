6304c392033db5e11864b16f0eaffc35abab2f32
levi <levisantosluna@gmail.com>
2021-07-12 22:25:44 -0300

    media ponderada

--- exercicios/algoritmo_15.rb
+++ exercicios/algoritmo_15.rb
@@ -1,1 +1,3 @@
 #Escreva um algoritmo que receba o nome de um aluno e 3 notas e faça a média ponderada, sendo o peso da primeira nota 3, segunda nota 4 e terceira nota 5.
+#Entrada: 6.8, 9.5, 4.2
+#Saída: Aluno: Fulano, Média: 6,61
